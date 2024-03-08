4476a5b1580b30db8789707727aaf38f14bc3518
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-03-08 19:51:49 +0530

    mongodb setup

---
+++ app/api/mongo/route.js
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+    const uri = "mongodb+srv://Aryan:Z12FtgHfkdH9hTxU@aryanmongo.yd2rer7.mongodb.net/";
+
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB server
+        await client.connect();
+
+        const database = client.db('aryan');
+        const movies = database.collection('inventory');
+
+        // Query for a movie that has the title 'Back to the Future'
+        const query = {};
+        const movie = await movies.findOne(query);
+
+        console.log(movie);
+        // Corrected syntax for NextResponse.json()
+        return NextResponse.json({ "a": 34, "movie": movie });
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
